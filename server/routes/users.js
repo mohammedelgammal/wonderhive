0fcfdbdc306d97caf3f4974805f4ae0aa8bedcef
mohammedelgammal <contact@mohammedelgammal.codes>
2023-07-06 06:01:36 +0300

    complete_ready_authentication_part

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,32 @@
 import express from "express";
 import UserModel from "../models/Users.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const router = express.Router();
+const usersRoute = express.Router();
 
-// routes
-router.post("/register", async (req, res) => {
+usersRoute.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const targetUser = await UserModel.findOne({ username });
   if (targetUser) return res.json({ message: "User already exists!" });
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new UserModel({ username, password: hashedPassword });
+  const newUser = new UserModel({
+    username: username,
+    password: hashedPassword,
+  });
   await newUser.save();
-  return res.json({ message: "User created successfully!" });
+  return res.json({ message: "User has been created successfully!" });
 });
 
-export default router;
+usersRoute.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const targetUser = await UserModel.findOne({ username });
+  if (!targetUser) return res.json({ message: "User does not exist!" });
+  const isPasswordValid = await bcrypt.compare(password, targetUser.password);
+  if (!isPasswordValid)
+    return res.json({ message: "Password is not correct!" });
+  const token = jwt.sign({ username }, process.env.SECRET);
+  return res.json({ message: "Login successfully!", username, token });
+});
+
+export default usersRoute;

8d35ce6c9d4eca2df6549970996aac5e0f45e0d5
mohammedelgammal <contact@mohammedelgammal.codes>
2023-07-08 04:19:53 +0300

    change_routing_components

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, Routes } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./data/routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Routes>
-      <RouterProvider router={router} />
-    </Routes>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

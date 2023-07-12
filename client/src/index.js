2de0275e51196e2c50f5d434e8d504a0a5ac8199
mohammedelgammal <contact@mohammedelgammal.codes>
2023-07-13 01:58:22 +0300

    binary_search_tree_initial_&_addChildNode

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
@@ -9,83 +9,46 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 class Node {
   constructor(value, parent) {
     this.value = value;
-    this.children = [];
     this.parent = parent;
+    this.rightChildNode = null;
+    this.leftChildNode = null;
   }
-  addChildNode(path) {
-    if (path.length) {
-      const segments = path.split("/");
-      const newPath = segments.slice(1).join("/");
-      const targetNode = this.children.find(
-        (node) => node.value === segments[0]
-      );
-      if (targetNode) {
-        targetNode.addChildNode(newPath);
-      } else {
-        const newNode = new Node(segments[0], this);
-        this.children.push(newNode);
-        newNode.addChildNode(newPath);
+
+  addChildNode(value) {
+    const newNode = new Node(value, this);
+    if (value >= this.value) {
+      if (this.rightChildNode) {
+        return this.rightChildNode.addChildNode(value);
       }
-    }
-  }
-  removeChildNode(path) {
-    if (path.length) {
-      const segments = path.split("/");
-      const targetNode = this.children.find(
-        (node) => node.value === segments[0]
-      );
-      if (targetNode) {
-        if (targetNode.value === segments[segments.length - 1]) {
-          const targetIndex = this.children.indexOf(targetNode);
-          return this.children.splice(targetIndex, 1);
-        }
-        const newPath = segments.slice(1).join("/");
-        targetNode.removeChildNode(newPath);
-      } else {
-        throw Error("Could not find the node");
+      this.rightChildNode = newNode;
+    } else {
+      if (this.leftChildNode) {
+        return this.leftChildNode.addChildNode(value);
       }
+      this.leftChildNode = newNode;
     }
-  }
-  findNode(value) {
-    for (const child of this.children) {
-      if (child.value === value) return child;
-      const grandChild = child.findNode(value);
-      if (grandChild) return grandChild;
-    }
-    return null;
-  }
-}
-class Tree {
-  constructor(rootValue) {
-    this.root = new Node(rootValue, null);
-  }
-  add(path) {
-    this.root.addChildNode(path);
-  }
-  remove(path) {
-    this.root.removeChildNode(path);
-  }
-  find(value) {
-    return this.root.findNode(value);
   }
 }
 
-const fileSystem = new Tree("/");
-fileSystem.add("public/index.js");
-fileSystem.add("documents/readme.md");
-fileSystem.add("documents/manifest.md");
-fileSystem.add("src/assets/css");
-fileSystem.add("src/components");
-fileSystem.add("src/containers");
-fileSystem.add("src/containers");
-fileSystem.add("src/containers");
-fileSystem.add("src/common");
+class BinaryTree {
+  constructor(value) {
+    this.root = new Node(value, null);
+  }
+  add(value) {
+    this.root.addChildNode(value);
+  }
+}
 
-fileSystem.add("src/common/navbar");
-fileSystem.add("src/common/navbar/navbar.jsx");
-console.log(fileSystem.find("components"));
+const newTree = new BinaryTree(10);
 
-console.log(fileSystem);
+newTree.add(11);
+newTree.add(20);
+newTree.add(9);
+newTree.add(5);
+newTree.add(21);
+newTree.add(8);
+
+console.log(newTree);
 
 root.render(
   <React.StrictMode>

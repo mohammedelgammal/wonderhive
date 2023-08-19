0602f4e2444cb254cd61a3398e0e7c5fd3c15ae9
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-19 23:16:57 +0300

    adding_login_feature_auth_layout

---
+++ client/src/Features/Login/Login.tsx
@@ -0,0 +1,8 @@
+// Styles
+import Style from "./Login.module.css";
+
+const Login: React.FC<{}> = (): React.ReactNode => {
+  return <div className={Style.login}></div>;
+};
+
+export default Login;
